09d3490a224bd7309932b005e8d7f7f84a36f153
CedarJin <cedarjys@gmail.com>
2022-10-14 17:34:01 +0800

    1.按行名匹配了注释 2.增加了注释，为注释增加了颜色，但不好看 3.重新针对注释排序了数据

--- energy_gene_heatmap.R
+++ energy_gene_heatmap.R
@@ -14,16 +14,43 @@ library(tidyr)
 row<-unite(data,"gene",X,X.1,sep=",  ", remove = F)
 new_data <- row[,4:5]
 rownames(new_data)<- row[,1]
-pheatmap(new_data,cluster_rows=FALSE, cluster_cols=FALSE,na_col = "grey90",
+
+
+
+annotation_row = data.frame(
+  Function_Classification = c("Glucogenesis", "Oxidative phosphorylation", "Glycolysis","Glycolysis",
+                              "Glucose transport","Glucose transport","Glycogenolysis","Glucose transport",
+                              "Glycolysis","Glycolysis","Glucose transport","Glycolysis",
+                              "Glycolysis","Glucose transport","Oxidative phosphorylation","Oxidative phosphorylation",
+                              "Glycolysis","Glycolysis","Glycogen biosynthesis"),
+  row.names = rownames(new_data))
+
+new_data2 <- merge(new_data,annotation_row,by="row.names",all = T)
+row.names(new_data2)<-new_data2[,1]
+new_data2<- new_data2[,2:4]
+#ordering
+library(dplyr)
+new_data2<-arrange(new_data2, Function_Classification)
+
+#install colorRampPalette related package
+install.packages('dichromat')
+library('dichromat')
+
+#ann_colors
+ann_colors<- list(
+  Function_Classification = c("Glucogenesis"= "#fef0d9", "Glucose transport"="#e0ecf4",
+                              "Glycogen biosynthesis"="#fdbb84","Glycogenolysis"="#9ebcda",
+                              "Glycolysis"="#e34a33","Oxidative phosphorylation"="#8856a7"))
+
+
+pheatmap(new_data2[,1:2],cluster_rows=F, cluster_cols=FALSE,na_col = "grey90",
          scale="column",
          color = colorRampPalette(c("#fff7f3", "#fa9fb5", "#7a0177"))(50),
          display_numbers = TRUE,
          angle_col = "45",
          border_color = "white",
          cellwidth = 20, cellheight = 20,
-         gaps_row = c(6,16),gaps_col = c(1,2),
+         gaps_row =c(6,16) ,gaps_col = c(1,2),
          fontsize_number = 6,
          )
 
-install.packages('dichromat')
-library('dichromat')
